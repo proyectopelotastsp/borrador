ec32e6c163abc3c09af74847eba982e0789fc9be
wilmer <66344475+wilmerfuturo@users.noreply.github.com>
2020-06-14 19:32:01 -0500

    trabajo al menu
    trabajo al menu

--- PelotasLocal/src/app/app.module.ts
+++ PelotasLocal/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './pages/nav/nav.component';
+import { NavComponent } from "./pages/nav/nav.component";
 import { MenuAsideComponent } from './pages/menu-aside/menu-aside.component';
 import { HeaderComponent } from './pages/header/header.component';
+
 
 @NgModule({
   declarations: [
--- PelotasLocal/src/app/pages/nav/nav.component.ts
+++ PelotasLocal/src/app/pages/nav/nav.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+    logo = "assets/imagenes/pelotas.jpg";
+    
   constructor() { }
 
   ngOnInit(): void {
